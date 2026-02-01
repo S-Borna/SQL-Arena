8b475419823bd87125180c3d3d624fc271b311f0
Said Ebadi <said@maketheplay.ai>
2026-02-01 01:43:24 +0100

    Fix: Load WASM locally for instant startup

--- src/engine/sqlRunner.ts
+++ src/engine/sqlRunner.ts
@@ -24,7 +24,7 @@ export async function initializeSqlJs(): Promise<void> {
   if (SQL) return;
 
   SQL = await initSqlJs({
-    locateFile: (file: string) => `https://sql.js.org/dist/${file}`
+    locateFile: (file: string) => `/${file}`
   });
 }
 
